bb120ca35f8e269ba776c113fb5a7dafc5399829
mazey <mazey@mazey.net>
2017-10-10 10:04:53 +0800

    code

--- test/39yst.php
+++ test/39yst.php
@@ -6,6 +6,21 @@ require dirname(__FILE__).'/../core/init.php';
 /* 不要删除这段注释 */
 
 $configs = array(
-    
+    'name' => '三九养生堂',
+    'tasknum' => 1,
+    'domains' => array(
+        'www.39yst.com',
+        '39yst.com'
+    ),
+    'scan_urls' => array(
+        'http://www.39yst.com'
+    ),
+    'list_url_regexes' => array(
+        'http://www.39yst.com/\w+/',
+        'http://www.39yst.com/\w+/\w+/'
+    ),
+    'content_url_regexes' => array(
+        'http://www.39yst.com/\w+/.+\.shtml',
+    )
 
-)
+);
